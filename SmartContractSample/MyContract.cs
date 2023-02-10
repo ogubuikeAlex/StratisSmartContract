38b210952571eb38425e88cb30b8695c8c3bdbcd
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-02-10 21:45:11 +0100

    Add project files.

---
+++ SmartContractSample/MyContract.cs
@@ -0,0 +1,21 @@
+﻿using Stratis.SmartContracts;
+using System;
+
+public class MyContract : SmartContract
+{
+    public MyContract(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+        SetOwner(Message.Sender);
+    }
+
+    private void SetOwner(Address owner)
+    {
+        PersistentState.SetAddress("owner", owner);
+    }
+
+    public Address GetOwner()
+    {
+        return PersistentState.GetAddress("owner");
+    }
+}
---
+++ StratisSmartContract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StratisSmartContract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
