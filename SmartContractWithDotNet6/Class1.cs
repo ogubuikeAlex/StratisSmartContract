8b745303355048c48536605efc594719cd318023
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-02-18 23:03:53 +0100

    chore: update .Net 6 contract code

---
+++ SmartContractWithDotNet6/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Stratis.SmartContracts;
+
+namespace SmartContractWithDotNet6
+{
+    /// <summary>
+    /// A basic "Hello World" smart contract
+    /// </summary>
+    public class Class1 : SmartContract
+    {
+        public Class1(ISmartContractState smartContractState) : base(smartContractState) 
+        {
+            SetOwner(Message.Sender);
+        }
+       
+        private void SetOwner(Address owner)
+        {
+            State.SetAddress("owner", owner);
+        }
+
+        public Address GetOwner()
+        {
+            return State.GetAddress("owner");
+        }
+    }
+}
